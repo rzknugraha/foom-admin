94b6a496136033b09e00f2795615981df0034e24
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2020-02-11 14:43:05 +0700

    first step login and layout

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(App\User::class, 1)->create();
     }
 }
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,58 @@
+    <!-- Footer -->
+    <footer class="site-footer">
+        <div class="site-footer-legal">© 2018 <a href="http://themeforest.net/item/remark-responsive-bootstrap-admin-template/11989202">Remark</a></div>
+        <div class="site-footer-right">
+          Crafted with <i class="red-600 wb wb-heart"></i> by <a href="https://themeforest.net/user/creation-studio">Creation Studio</a>
+        </div>
+      </footer>
+      <!-- Core  -->
+      <script src="{{ asset ('global/vendor/babel-external-helpers/babel-external-helpers.js') }}"></script>
+      <script src="{{ asset ('global/vendor/jquery/jquery.js') }}"></script>
+      <script src="{{ asset ('global/vendor/popper-js/umd/popper.min.js') }}"></script>
+      <script src="{{ asset ('global/vendor/bootstrap/bootstrap.js') }}"></script>
+      <script src="{{ asset ('global/vendor/animsition/animsition.js') }}"></script>
+      <script src="{{ asset ('global/vendor/mousewheel/jquery.mousewheel.js') }}"></script>
+      <script src="{{ asset ('global/vendor/asscrollbar/jquery-asScrollbar.js') }}"></script>
+      <script src="{{ asset ('global/vendor/asscrollable/jquery-asScrollable.js') }}"></script>
+      <script src="{{ asset ('global/vendor/ashoverscroll/jquery-asHoverScroll.js') }}"></script>
+      
+      <!-- Plugins -->
+      <script src="{{ asset ('global/vendor/switchery/switchery.js') }}"></script>
+      <script src="{{ asset ('global/vendor/intro-js/intro.js') }}"></script>
+      <script src="{{ asset ('global/vendor/screenfull/screenfull.js') }}"></script>
+      <script src="{{ asset ('global/vendor/slidepanel/jquery-slidePanel.js') }}"></script>
+          <script src="{{ asset ('global/vendor/skycons/skycons.js') }}"></script>
+          <script src="{{ asset ('global/vendor/chartist/chartist.min.js') }}"></script>
+          <script src="{{ asset ('global/vendor/chartist-plugin-tooltip/chartist-plugin-tooltip.js') }}"></script>
+          <script src="{{ asset ('global/vendor/aspieprogress/jquery-asPieProgress.min.js') }}"></script>
+          <script src="{{ asset ('global/vendor/jvectormap/jquery-jvectormap.min.js') }}"></script>
+          <script src="{{ asset ('global/vendor/jvectormap/maps/jquery-jvectormap-au-mill-en.js') }}"></script>
+          <script src="{{ asset ('global/vendor/matchheight/jquery.matchHeight-min.js') }}"></script>
+      
+      <!-- Scripts -->
+      <script src="{{ asset ('global/js/Component.js') }}"></script>
+      <script src="{{ asset ('global/js/Plugin.js') }}"></script>
+      <script src="{{ asset ('global/js/Base.js') }}"></script>
+      <script src="{{ asset ('global/js/Config.js') }}"></script>
+      
+      <script src="{{ asset('js/Section/Menubar.js') }}"></script>
+      <script src="{{ asset('js/Section/GridMenu.js') }}"></script>
+      <script src="{{ asset('js/Section/Sidebar.js') }}"></script>
+      <script src="{{ asset('js/Section/PageAside.js') }}"></script>
+      <script src="{{ asset('js/Plugin/menu.js') }}"></script>
+      
+      <script src="{{ asset ('global/js/config/colors.js') }}"></script>
+      <script src="{{ asset('js/config/tour.js') }}"></script>
+      <script>Config.set('assets', '{{ asset ("assets") }}');</script>
+      
+      <!-- Page -->
+      <script src="{{ asset('js/Site.js') }}"></script>
+      <script src="{{ asset ('global/js/Plugin/asscrollable.js') }}"></script>
+      <script src="{{ asset ('global/js/Plugin/slidepanel.js') }}"></script>
+      <script src="{{ asset ('global/js/Plugin/switchery.js') }}"></script>
+          <script src="{{ asset ('global/js/Plugin/matchheight.js') }}"></script>
+          <script src="{{ asset ('global/js/Plugin/jvectormap.js') }}"></script>
+      
+          <script src="{{ asset('js/dashboard/v1.js') }}"></script>
+    </body>
+  </html>
